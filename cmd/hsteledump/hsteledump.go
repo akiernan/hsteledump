1351ce98dc7df1e908ea4227b4b06c4835d13a49
Alex Kiernan <alex.kiernan@gmail.com>
2018-12-30 18:39:02 +0000

    Change to take a list of directories, adapt to API changes

--- cmd/hsteledump/hsteledump.go
+++ cmd/hsteledump/hsteledump.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strconv"
+	"os"
 
 	"github.com/tokuhirom/go-hsperfdata/hsperfdata"
 )
@@ -12,11 +12,9 @@ import (
 func dumpStat(result *hsperfdata.Result) {
 	proc_name := result.GetProcName()
 	for key, value := range result.GetMap() {
-		if str, ok := value.(string); ok {
-			_, err := strconv.ParseFloat(str, 64)
-			if err == nil {
-				fmt.Printf("%s %v proc_name=\"%s\"\n", key, value, proc_name)
-			}
+		_, is_string := value.(string)
+		if !is_string {
+			fmt.Printf("%s %v proc_name=\"%v\"\n", key, value, proc_name)
 		}
 	}
 }
@@ -30,21 +28,19 @@ func main() {
 		return
 	}
 
-	repo, err := hsperfdata.NewUser("root")
-	if err != nil {
-		log.Fatal("new", err)
-	}
-	files, err := repo.GetFiles()
-	if err != nil {
-		log.Fatal("repo", err)
-	}
-
-	for _, f := range files {
-		result, err := f.Read()
+	for _, dir := range os.Args {
+		repo, err := hsperfdata.NewDir(dir)
+		if err != nil {
+			log.Fatal("new", err)
+		}
+		files, err := repo.GetFiles()
 		if err == nil {
-			dumpStat(result)
-		} else {
-			fmt.Printf("%s unknown\n", f.GetPid())
+			for _, f := range files {
+				result, err := f.Read()
+				if err == nil {
+					dumpStat(result)
+				}
+			}
 		}
 	}
 }
